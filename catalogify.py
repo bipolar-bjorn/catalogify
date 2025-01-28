7f7c914136ee4ef2c6ed216217990cb18c5ba85a
Pavel <psokolov@mavs.tech>
2025-01-28 23:25:32 +0400

    a skeleton to parser

--- catalogify.py
+++ catalogify.py
@@ -6,7 +6,10 @@ import logging
 from typing import Final
 from pathlib import Path
 
+import pandas as pd
 from tqdm import tqdm
+from mutagen.easyid3 import EasyID3
+from mutagen.id3._util import ID3NoHeaderError
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s %(module)s %(levelname)s %(message)s"
@@ -23,14 +26,30 @@ def get_files_from_directory(path: Path) -> set:
         path.rglob("*"),
         desc="Collecting files",
     ):
+        if file.name.startswith("."):
+            continue
+
         if file.suffix in MUSIC_FORMATS:
             files.add(file)
     return files
 
 
-def process_mp3() -> None:
+def process_mp3(file: Path) -> pd.DataFrame:
     """Process .mp3 files and get tags"""
-    pass
+    try:
+        track = EasyID3(file)
+        tags = {
+            "file_path": str(file.absolute()),
+            "artist": track.get("artist", [""])[0],
+            "album": track.get("album", [""])[0],
+            "title": track.get("title", [""])[0],
+            "year": track.get("date", [""])[0],
+            "genre": track.get("genre", [""])[0],
+        }
+        return pd.DataFrame([tags])
+    except ID3NoHeaderError:
+        logging.warning(f"File {file} doesn't start with an ID3 tag and will be skipped.")
+        return pd.DataFrame(columns=["file_path", "artist", "album", "title", "year", "genre"])
 
 
 def process_wav() -> None:
@@ -51,16 +70,19 @@ def process_ogg() -> None:
 def main(path: Path, file: Path) -> None:
     """Main function that initiate parsing"""
     files = get_files_from_directory(path=path)
-    with file.open() as document:
-        document.write_text(files)
-
+    df = pd.DataFrame(columns=["file_path", "artist", "album", "year", "genre"])
+    for file_path in files:
+        if file_path.suffix == ".mp3":
+            data = process_mp3(file=file_path)
+            df = pd.concat([df, data], ignore_index=True)
+    df.to_excel(file.with_suffix(".xlsx"), index=False)
 
 
 if __name__ == "__main__":
     MUSIC_FORMATS: Final = frozenset([".mp3", ".wav", ".flac", ".ogg", ".m4a"])
     COLLECTION_DIR_PATH: Final = Path(__file__).parent.joinpath("catalog")
     COLLECTION_FILE_PATH: Final = Path(__file__).parent.joinpath(
-        COLLECTION_DIR_PATH, "collection.xls"
+        COLLECTION_DIR_PATH, "collection.csv"
     )
 
     while True:
@@ -72,6 +94,5 @@ if __name__ == "__main__":
 
     if not COLLECTION_DIR_PATH.exists():
         COLLECTION_DIR_PATH.mkdir()
-        COLLECTION_FILE_PATH.touch()
 
     main(path=directory, file=COLLECTION_FILE_PATH)
